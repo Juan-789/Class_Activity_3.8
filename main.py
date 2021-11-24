14c25d73f62003b8fd5e0364504a26c1fbe5dc63
Juan Marulanda <79994699+Juan-789@users.noreply.github.com>
2021-11-24 16:39:39 +0000

    Comments

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
+#Write a program that asks a user for three integers. Print the smallest value.
 print("Give me three integers and I will tell you which is the smallest ")
 firstInt =  int(input("Enter First Integer "))
 secondInt = int (input("Enter Second Integer "))
 thirdInt = int(input("Enter Third Integer "))
+#Collects data
 if firstInt < secondInt and firstInt < thirdInt:
   print(f"{firstInt} is the smallest value ")
 elif firstInt > secondInt and secondInt < thirdInt:
   print(f"{secondInt} is the smallest value ")
 elif thirdInt < secondInt and firstInt > thirdInt:
   print(f"{thirdInt} is the smallest value ")
+#prints smallest value
